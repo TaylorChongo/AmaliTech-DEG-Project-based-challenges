8dba256aa6b198a8a20016848df04899bfba3e6a
TaylorChongo <taylorchongo01@icloud.com>
2026-04-24 20:20:33 +0200

    feat: add process-payment endpoint

--- backend/Idempotency-gateway/main.py
+++ backend/Idempotency-gateway/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Body
+from pydantic import BaseModel
 
 app = FastAPI(title="Idempotency Gateway API")
+
+class PaymentRequest(BaseModel):
+    amount: float
+    currency: str
 
 @app.get("/")
 async def root():
     return {"message": "API running"}
+
+@app.post("/process-payment")
+async def process_payment(
+    payment: PaymentRequest,
+    idempotency_key: str = Header(..., alias="Idempotency-Key")
+):
+    return {"message": "processing..."}
